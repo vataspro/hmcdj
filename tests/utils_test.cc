b24d5423685de9e5fdcc50eb948771f2566ef421
Ed Bennett <e.j.bennett@swansea.ac.uk>
2025-08-28 14:31:46 +0100

    don't litter /tmp with outputs from the utils test

--- tests/utils_test.cc
+++ tests/utils_test.cc
@@ -3,6 +3,8 @@
 
 #include <algorithm>  // needed for count
 
+#include "run_test_helpers.h"
+
 // The file used to test the RNG seed
 #define TESTSEEDFILE \
   std::string(TOP_SRCDIR) + "/example_tracks/NoParamsTrack.yaml"
@@ -27,6 +29,17 @@ bool StrIsDigits(const std::string str) {
 
 // Check that the ensemble reader loads the correct lattice dimensions string
 TEST(UtilTest, EnsembleReaderTest) {
+  TemporaryDirectory tmpDir("hmcdj_utiltest");
+
+  // Ensure that we go down the home directory code path by suppressing any base
+  // path from the environment
+  TemporaryEnvironmentSuppress baseDir("HMCDJ_BASE_PATH");
+
+  // Ensure that we don't litter the user's home directory by creating a
+  // temporary fake home directory
+  TemporaryEnvironmentOverride homeDir("HOME",
+                                       tmpDir.getDirectoryPath().string());
+
   // Load the test "track.yaml" file
   std::string filename = TESTSEEDFILE;
   EnsembleReader reader("NoParams", filename, {});
